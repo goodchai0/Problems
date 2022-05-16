6e4233678626156371d88f2740722bb89b75b9cf
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-05-16 12:29:52 +0530

    Time: 115 ms (73.51%), Space: 59.8 MB (53.52%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int height (TreeNode* root)
+  
+    int height(TreeNode* root)
     {
-        if (!root)
+        if(!root)
             return 0;
-        return max(height(root->left), height(root->right)) + 1;
+        return max(height(root->left),height(root->right))+1;
     }
-    int sum = 0;
-    int sum_at_k(TreeNode* root, int k)
-    {
+//     int sum = 0;
+//     int sum_at_k(TreeNode* root, int k)
+//     {
         
-        if (!root)
-            return sum;
-        if (k == 0)
-            sum = sum + root->val;
-        sum_at_k(root->left, k - 1);
-        sum_at_k(root->right, k - 1);
+//         if (!root)
+//             return sum;
+//         if (k == 0)
+//             sum = sum + root->val;
+//         sum_at_k(root->left, k - 1);
+//         sum_at_k(root->right, k - 1);
+//         return sum;
+//     }
+    
+    int sum=0;
+    int sum_at_k(TreeNode* root,int k)
+    {
+        if(!root)
+            return 0;
+        
+        if(k==0)
+            sum=sum+root->val;
+        sum_at_k(root->left,k-1);
+        sum_at_k(root->right,k-1);
         return sum;
     }
     
-    int deepestLeavesSum(TreeNode* root) 
+    
+    int deepestLeavesSum(TreeNode*root)
     {
-        int h = height(root);
-        int sum = sum_at_k(root, h - 1);
+        int h=height(root);
+        int sum=sum_at_k(root,h-1);
         return sum;
     }
 };
+
+
