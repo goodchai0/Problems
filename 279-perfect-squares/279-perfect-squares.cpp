93d3c642ed0984dba1600e3cb77b6730d6489e32
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-05-28 00:42:51 +0530

    Time: 110 ms (79.17%), Space: 6 MB (92.95%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,24 +1,42 @@
+// class Solution {
+// public:
+//     vector<int>dp;
+//     int solve(int n)
+//     {
+//         if(n<=3)
+//             return n;
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         int ans=n;
+//         for(int i=1;i*i<=n;i++)
+//         {
+//             ans=min(ans,1+solve(n-i*i));
+            
+//         }
+//         dp[n]=ans;
+//         return ans;
+//     }
+//     int numSquares(int n) {
+//         dp.resize(n+1,-1);
+//         int ans=solve(n);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int>dp;
-    int solve(int n)
-    {
-        if(n<=3)
-            return n;
-        if(dp[n]!=-1)
-            return dp[n];
-        int ans=n;
-        for(int i=1;i*i<=n;i++)
-        {
-            ans=min(ans,1+solve(n-i*i));
-            
-        }
-        dp[n]=ans;
-        return ans;
-    }
     int numSquares(int n) {
-        dp.resize(n+1,-1);
-        int ans=solve(n);
-        return ans;
+        int dp[n+1];
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=i;
+            for(int j=1;j*j<=i;j++)
+            {
+                int sq=j*j;
+                dp[i]=min(dp[i],1+dp[i-sq]);
+            }
+        }
+        return dp[n];
     }
 };
