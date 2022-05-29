3f55057a7ebc629e9f714e4445086a4c5a588c62
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-05-30 00:31:44 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-   
-    int f(vector<int>nums,int ind,vector<int>&dp)
-    {
-        if(ind==0)return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1)
-            return dp[ind];
-        int pick=nums[ind]+f(nums,ind-2,dp);
-        int notpick=f(nums,ind-1,dp);
-        
-        return dp[ind]=max(pick,notpick);
-    }
     int rob(vector<int>& nums) {
-        int index=nums.size();
-         vector<int> dp(index,-1);
-        return f(nums,index-1,dp);
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
         
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1)
+                take+=prev2;
+            int notTake=prev;
+            
+            int curr=max(take,notTake);
+          
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> dp;
+//     int f(vector<int>nums,int ind)
+//     {
+//         if(ind==0)return nums[ind];
+//         if(ind<0) return 0;
+//         if(dp[ind]!=-1)
+//             return dp[ind];
+//         int pick=nums[ind]+f(nums,ind-2);
+//         int notpick=f(nums,ind-1);
+        
+//         return dp[ind]=max(pick,notpick);
+//     }
+//     int rob(vector<int>& nums) {
+//         int index=nums.size()-1;
+//         dp.resize(index+1,-1);
+//         return f(nums,index);
+        
+//     }
+// };
