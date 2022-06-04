8c3a46b1b256f21b11b54803983833211a21611e
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-06-04 12:41:17 +0530

    Time: 38 ms (29.08%), Space: 15.5 MB (10.47%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,25 +11,52 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
         if(head==NULL)
             return NULL;
-        ListNode* h=head;
-        
-        
-        while( h->next!=NULL )
-        {
-    
-            if( h!=NULL && h->next->val==val)
-            {
-                h->next=h->next->next;
-                cout<<h->val<<" ";
-            }
-            else h = h->next;
-                 
-        }
         if(head->val==val)
-            head=head->next;
-        return head;
+            return removeElements(head->next,val);
+        else
+        {
+            head->next=removeElements(head->next,val);
+            return head;
+        }
+        
     }
 };
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+        
+//         if(head==NULL)
+//             return NULL;
+//         ListNode* h=head;
+        
+        
+//         while( h->next!=NULL )
+//         {
+    
+//             if( h!=NULL && h->next->val==val)
+//             {
+//                 h->next=h->next->next;
+//                 cout<<h->val<<" ";
+//             }
+//             else h = h->next;
+                 
+//         }
+//         if(head->val==val)
+//             head=head->next;
+//         return head;
+//     }
+// };
