a455888c9587717af6572641781cd847258f1bd3
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-06-21 01:55:16 +0530

    Time: 61 ms (20.88%), Space: 18.4 MB (15.78%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int r=board.size();
+        int steps=0;
+        
+        queue<int>q;
+        q.push(1);
+        
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                int t=q.front();
+                q.pop();
+                if(t==r*r)return steps;
+                for(int i=1;i<=6;i++)
+                {
+                    int next_step=t+i;
+                    if(next_step>r*r)break;
+                    
+                    auto pos=findCoordinates(r,next_step);
+                    int row=pos[0];
+                    int col=pos[1];
+                    
+                    if(board[row][col]!=-1)
+                        next_step=board[row][col];
+                    if(board[row][col]!=r*r+1)
+                    {
+                        q.push(next_step);
+                        board[row][col]=r*r+1;
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+        
+    }
+    
+    vector<int> findCoordinates(int row,int next_step)
+    {
+        int x=(next_step-1)/row;
+        int y=(next_step-1)%row;
+        if(x%2==1)y=row-1-y;
+        x=row-1-x;
+        return {x,y};
+    }
+};
