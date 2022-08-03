3ed0872a8a26472d6eede00894e5365b779eef87
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-08-04 00:37:38 +0530

    Time: 87 ms (99.66%), Space: 33.9 MB (88.46%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.cpp
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -1,29 +1,60 @@
 class MyCalendarTwo {
 public:
-    map<int,int>m;
+    vector<pair<int,int>>events;
+    vector<pair<int,int>>doublebooking;
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        m[start]++;
-        m[end]--;
-        int sum=0;
-        for(auto it:m){
-            sum+=it.second;
-            if(sum>=3)
+        for(auto p:doublebooking)
+        {
+            if(p.second>start && p.first<end)
             {
-                m[start]--;
-                m[end]++;
                 return false;
             }
         }
+        
+        for(auto p:events)
+        {
+            if(p.second>start && p.first<end)
+            {
+                doublebooking.push_back({max(start,p.first),min(end,p.second)});
+            }
+        }
+        events.push_back({start,end});
         return true;
     }
+    
+    
 };
 
-/**
- * Your MyCalendarTwo object will be instantiated and called as such:
- * MyCalendarTwo* obj = new MyCalendarTwo();
- * bool param_1 = obj->book(start,end);
- */
+// class MyCalendarTwo {
+// public:
+//     map<int,int>m;
+//     MyCalendarTwo() {
+        
+//     }
+    
+//     bool book(int start, int end) {
+//         m[start]++;
+//         m[end]--;
+//         int sum=0;
+//         for(auto it:m){
+//             sum+=it.second;
+//             if(sum>=3)
+//             {
+//                 m[start]--;
+//                 m[end]++;
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// /**
+//  * Your MyCalendarTwo object will be instantiated and called as such:
+//  * MyCalendarTwo* obj = new MyCalendarTwo();
+//  * bool param_1 = obj->book(start,end);
+//  */
