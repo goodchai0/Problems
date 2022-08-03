5282c14a870a5fa4d0d14c942ed4df2c12d5bad5
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-08-03 21:56:50 +0530

    Time: 97 ms (93.81%), Space: 39 MB (31.77%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,22 +1,40 @@
 class MyCalendar {
 public:
-        vector<pair<int,int>>m;
+    set<pair<int,int>>s;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        for(auto it:m){
-            if(it.first<end && start<it.second)
-                return false;
-        }
-        m.push_back({start,end});
-        return true;
+        auto it=s.upper_bound({start,end});
+        if(it!=s.end()&&it->second<end)
+            return false;
+        s.insert({end,start});
+            return true;
     }
 };
 
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
+
+
+// class MyCalendar {
+// public:
+//         vector<pair<int,int>>m;
+//     MyCalendar() {
+        
+//     }
+    
+//     bool book(int start, int end) {
+//         for(auto it:m){
+//             if(it.first<end && start<it.second)
+//                 return false;
+//         }
+//         m.push_back({start,end});
+//         return true;
+//     }
+// };
+
+// /**
+//  * Your MyCalendar object will be instantiated and called as such:
+//  * MyCalendar* obj = new MyCalendar();
+//  * bool param_1 = obj->book(start,end);
+//  */
