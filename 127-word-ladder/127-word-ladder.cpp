47cc829700cb86583ee337cfa7562661f60d1824
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-08-15 03:30:42 +0530

    Time: 338 ms (52.83%), Space: 40.3 MB (9.75%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        map<string,vector<string>>m;
+        int msize=beginWord.size();
+        
+        for(int i=0;i<wordList.size();i++){
+            string str=wordList[i];
+            
+            for(int j=0;j<msize;j++){
+                string x=str.substr(0,j)+"*"+str.substr(j+1,msize-j-1);
+                m[x].push_back(str);
+            }
+        }
+        
+        string str=beginWord;
+        for(int j=0;j<msize;j++ ){
+            string x=str.substr(0,j)+"*"+str.substr(j+1,msize-j-1);
+            m[x].push_back(str);
+        }
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        set<string>visited;
+        visited.insert(beginWord);
+        
+        while(!q.empty()){
+            string str=q.front().first;
+            int level=q.front().second;
+            q.pop();
+        for(int j=0;j<msize;j++){
+            string p=str.substr(0,j)+"*"+str.substr(j+1,msize-j-1);
+            vector<string>v=m[p];
+            
+            for(string x:v){
+                if(visited.find(x)==visited.end()){
+                    q.push({x,level+1});
+                    visited.insert(x);
+                    if(x==endWord)return level+1;
+                }
+            }
+        }
+        }
+        return 0;
+    }
+};
