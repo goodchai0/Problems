8b419f90c6f070c46bda66badaec9ce1b5f2da30
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-08-17 00:56:20 +0530

    Time: 8 ms (96.15%), Space: 8.6 MB (23.52%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,20 +2,41 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char,int>m1;
-        multimap<int,char,greater<int>>m2;
+        priority_queue<pair<int,char>>maxHeap;
         string st="";
         int i,j=0;
         for(auto x:s)
             m1[x]++;
         
-        for(auto it=m1.begin();it!=m1.end();it++)
-            m2.insert({it->second,it->first});
-        for(auto it=m2.begin();it!=m2.end();it++){
-            for(auto i=0;i<it->first;i++){
-                st+=it->second;
-               
-            }
+        for(auto x:m1)
+            maxHeap.push({x.second,x.first});
+        while(!maxHeap.empty()){
+            st+=string(maxHeap.top().first,maxHeap.top().second);
+            maxHeap.pop();
         }
+        
         return st;
     }
 };
+
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char,int>m1;
+//         multimap<int,char,greater<int>>m2;
+//         string st="";
+//         int i,j=0;
+//         for(auto x:s)
+//             m1[x]++;
+        
+//         for(auto it=m1.begin();it!=m1.end();it++)
+//             m2.insert({it->second,it->first});
+//         for(auto it=m2.begin();it!=m2.end();it++){
+//             for(auto i=0;i<it->first;i++){
+//                 st+=it->second;
+               
+//             }
+//         }
+//         return st;
+//     }
+// };
