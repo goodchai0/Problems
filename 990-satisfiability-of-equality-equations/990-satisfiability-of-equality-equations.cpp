82ddb3aa23d26f11fccdb5e47bfc38be90a3e1e3
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-09-26 22:06:29 +0530

    Time: 7 ms (79.10%), Space: 11.2 MB (88.77%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,54 +1,19 @@
 class Solution {
 public:
+    vector<int> parent;
+    int find(int node){
+        return parent[node]==-1?node:parent[node]=find(parent[node]);
+    }
+    void takeUnion(int a,int b){
+        int x=find(a),y=find(b);
+        if(x!=y) parent[x]=y;
+    }
     bool equationsPossible(vector<string>& equations) {
-        vector<vector<int>>graph(26);
-        
-        for(auto eqn:equations)
-        {
-            if(eqn.substr(1,2) == "==")
-            {
-                int x=eqn[0]-'a';
-                int y=eqn[3]-'a';
-                graph[x].push_back(y);
-                graph[y].push_back(x);
-            }
-        }
-        
-        vector<int>color(26,0);
-        
-        int t=0;
-        for(int start=0;start<26;start++)
-        {
-            if(color[start]==0)
-            {
-                t++;
-                stack<int>st;
-                st.push(start);
-                while(!st.empty())
-                {
-                    int node=st.top();
-                    st.pop();
-                    for(auto nei:graph[node])
-                    {
-                        if(color[nei]==0)
-                        {
-                            color[nei]=t;
-                            st.push(nei);
-                        }
-                    }
-                }
-            }
-        }
-        for(auto eqn:equations)
-        {
-            if(eqn.substr(1,2) == "!=")
-            {
-                int x=eqn[0]-'a';
-                int y=eqn[3]-'a';
-                if(x==y||color[x]!=0 && color[x]==color[y])
-                    return false;
-            }
-        }
+        parent=vector<int>(26,-1);
+        for(auto eq:equations)
+            if(eq[1]=='=') takeUnion(eq[0]-'a',eq[3]-'a');
+        for(auto eq:equations)
+            if(eq[1]=='!' && find(eq[0]-'a') == find(eq[3]-'a'))return false;
         return true;
     }
 };
