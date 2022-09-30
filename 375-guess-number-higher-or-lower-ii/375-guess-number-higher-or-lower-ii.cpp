fa95bc002b899fa93ea05a394420ec6064568f28
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-09-30 12:17:22 +0530

    Time: 128 ms (79.20%), Space: 8.3 MB (45.33%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,19 +1,45 @@
+// class Solution {
+// public:
+//     int solve(int start,int end,vector<vector<int>>&dp){
+//         if(start>=end)
+//             return 0;
+//         if(dp[start][end]!=-1)return dp[start][end];
+        
+//         int maxi=INT_MAX;
+//         for(int i=start;i<=end;i++){
+//             maxi=min(maxi,i+max(solve(start,i-1,dp),solve(i+1,end,dp)));
+//         }
+//         return dp[start][end]=maxi;
+//     }
+
+//     int getMoneyAmount(int n) {
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+//         return solve(1,n,dp);
+//     }
+// };
+
 class Solution {
 public:
-    int solve(int start,int end,vector<vector<int>>&dp){
-        if(start>=end)
-            return 0;
-        if(dp[start][end]!=-1)return dp[start][end];
+    int solveTab(int n){
+        vector<vector<int>>dp(n+2,vector<int>(n+1,0));
         
-        int maxi=INT_MAX;
-        for(int i=start;i<=end;i++){
-            maxi=min(maxi,i+max(solve(start,i-1,dp),solve(i+1,end,dp)));
+        for(int start=n;start>=1;start--){
+            for(int end=start;end<=n;end++){
+                if(start==end)
+                    continue;
+                else{
+                    int maxi=INT_MAX;
+                    for(int i=start;i<=end;i++){
+                        maxi=min(maxi,i+max(dp[start][i-1],dp[i+1][end]));
+                    }
+                   dp[start][end]=maxi;
+                }
+            }
         }
-        return dp[start][end]=maxi;
+        return dp[1][n];
     }
-
+    
     int getMoneyAmount(int n) {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(1,n,dp);
+        return solveTab(n);
     }
 };
