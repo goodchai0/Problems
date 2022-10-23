2c5cfe6b5ca43a90437102bc8ea562238ff31b49
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-10-24 00:17:29 +0530

    Time: 185 ms (9.97%), Space: 33 MB (13.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
         set<int>s;
-        unordered_map<int,int>mp;
+        int sum1=accumulate(nums.begin(),nums.end(),0);
         for(auto x:nums){
             s.insert(x);
-            mp[x]++;
         }
-        int x,y;
-        for(int i=1;i<=nums.size();i++){
-            if(s.find(i)==s.end()){
-                y=i;
-            }
-            if(mp[i]==2){
-                x=i;
-            }
-        }
+        int sum2=accumulate(s.begin(),s.end(),0);
         
-        vector<int>v;
-        v.push_back(x);
-        v.push_back(y);
+        int x=(n*(n+1))/2-sum2;
+        int y=x+sum1-(n*(n+1))/2;
         
-        return v;
+        return {y,x};
     }
 };
+
+
+//class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& nums) {
+//         set<int>s;
+//         unordered_map<int,int>mp;
+//         for(auto x:nums){
+//             s.insert(x);
+//             mp[x]++;
+//         }
+//         int x,y;
+//         for(int i=1;i<=nums.size();i++){
+//             if(s.find(i)==s.end()){
+//                 y=i;
+//             }
+//             if(mp[i]==2){
+//                 x=i;
+//             }
+//         }
+        
+//         vector<int>v;
+//         v.push_back(x);
+//         v.push_back(y);
+        
+//         return v;
+//     }
+// };
