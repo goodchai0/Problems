145510fd28e4aa863388d31f74363683409cdcb1
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-10-25 19:07:29 +0530

    Time: 11 ms (58.33%), Space: 6 MB (58.97%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -3,19 +3,18 @@ public:
     long long mod = 1e9 + 7;
     
     int dp[10004];
-    int helper(int i,int n){
-        if(i>n)
-            return 1;
-        // ans++;
-        if(dp[i]!=-1)
-            return dp[i];
-        int include=helper(i+1,n);
-        int exclude=helper(i+2,n);
-        return dp[i]=(include%mod+exclude%mod)%mod;
+    int helper(int n){
+        
+        dp[0]=1,dp[1]=2;
+        for(int i=2;i<=n;i++){
+            dp[i]+=dp[i-1]%mod;
+            dp[i]+=dp[i-2]%mod;
+        }
+        return dp[n]%mod;
     }
     int countHousePlacements(int n) {
-        memset(dp,-1,sizeof(dp));
-        int ans = helper(1, n);
+        memset(dp,0,sizeof(dp));
+        int ans = helper(n);
         
         return (ans % mod * ans % mod) % mod;
     }
