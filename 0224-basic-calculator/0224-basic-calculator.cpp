0d92d1c74c5e1266646729e89757b672004f5036
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-11-20 18:53:07 +0530

    Time: 7 ms (94.36%), Space: 8 MB (93.14%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     int calculate(string s) {
         
-        stack<pair<int,int>>st;
+        stack<pair<int,int>>st; //sum,sign;
         long long int sum=0;
         
-        int sign=+1;
+        int sign=1;
         
         for(int i=0;i<s.size();i++)
         {
             char ch=s[i];
+            
+            // since its a string.
+            //-111+10
+            //
             if(isdigit(ch))
             {
                 long long int num=0;
                 while(i<s.size() && isdigit(s[i]))
                 {
-                    num=(num*10)+s[i]-'0';
+                    num=(num*10)+s[i]-'0';//'0' to convert it into int;
                     i++;
                 }
-                i--;
+                i--; // because of while loop;
                 sum+=(num*sign);
                 sign=1;
             }
             else if(ch=='(')
             {
-                st.push(make_pair(sum,sign));
+                st.push(make_pair(sum,sign));//pushing the current sum with sign
                 sum=0;
                 sign=1;
             }
@@ -42,11 +46,38 @@ public:
     }
 };
 
+
+// 1+11-3=9
+// 14
+
+//     9+14
+//     23
+    
+    
+
+
 // "(1+(4+5+2)-3)+(6+8)"
 
 // when first bracket will start sum=0;
 // adding sum+=1;
 
+// sum=1;
+// stack(sum);
+// sum=0
+    
+// 4+5+2=11
+// sum=9;
+
+// stack mai jo | previous sum +=present sum;
+// sum=1+11=12;
+
+
+//    sign=-1;
+// sum=sum+3*sign;
+    
+
+
+    
 // when second '(' will start 
 // we will store the sign and sum at that present state in the stack.
 // then we will make
@@ -59,7 +90,8 @@ public:
 // we will add the previous sum pushed into the stack to the present sum.
 
     
-// consider example: (1+2)-(2+1);
+// consider example: (1+2)-(2+1);// sign -ve present sum* sign;
+// //prevuious sum + current sum*sign;
 // the minus sign in the need to addressed.
 // so we will also store the sign in the stack with the current sum.
 
