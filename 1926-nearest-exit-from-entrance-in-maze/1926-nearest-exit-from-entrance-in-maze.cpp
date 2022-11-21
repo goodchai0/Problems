5829a6ce868f30b57e81148ed251d0b01c611b77
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-11-21 20:53:33 +0530

    Time: 225 ms (74.95%), Space: 29.9 MB (86.91%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
     int n,m;
-    int dx[4]={0,0,1,-1};
-    int dy[4]={1,-1,0,0}; // (0,0)-> (0,1),(0,-1),(1,0),(-1,0);
-    
     bool isBorder(int i,int j){
-        return (i==0 || i==m-1 ||j==0 || j==n-1);
+        return (i==0 || i==m-1 || j==0 || j==n-1);
     }
-    bool valid (int r,int c){
-        return (r>=0 && c>=0 && r<m && c<n);
+    
+    bool valid(int r,int c){
+        return (r>=0 && c>=0 && r<m && n>c);
     }
-    int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
+    
+    int dx[4]={0,0,1,-1};
+    int dy[4]={1,-1,0,0} ; // (0,0)->(0,1),(0,-1),(1,0),(-1,0);
+    
+    
+
+    
+    int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
+        
         m=maze.size(),n=maze[0].size();
-        
-        if(isBorder(ent[0],ent[1]))
-            maze[ent[0]][ent[1]]='+';
-        
         int ans=0;
+        if(isBorder(e[0],e[1]))
+            maze[e[0]][e[1]]='+';
         queue<pair<int,int>>q;
-        q.push({ent[0],ent[1]});
+        q.push({e[0],e[1]});
         while(!q.empty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 auto top=q.front();
                 q.pop();
-                int x=top.first, y=top.second;
-                if(maze[x][y]!='+' && isBorder(x,y))
+                int x=top.first,y=top.second;
+                if( maze[x][y]!='+' && isBorder(x,y))
                     return ans;
                 
                 for(int j=0;j<4;j++){
                     int r=x+dx[j];
                     int c=y+dy[j];
+                    
                     if(valid(r,c) && maze[r][c]=='.'){
                         q.push({r,c});
                         maze[r][c]='*';
                     }
                 }
+                    
             }
             ans++;
         }
