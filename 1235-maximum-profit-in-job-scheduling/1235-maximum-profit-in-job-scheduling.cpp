e54f6cebac9389ad05c2b0835460054cf8a90dcd
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-11-26 11:36:40 +0530

    Time: 225 ms (78.95%), Space: 67.7 MB (19.64%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,44 +1,44 @@
 class Solution {
-public:
+   public:
+
+  int getNextJob(vector < vector < int >> & jobs, int idx,const int & n) 
+  {
+     for (int i = idx + 1; i < n; i++) {
+        if (jobs[idx][1] <= jobs[i][0]) return i;
+     }
+     return n;
+  }
     
-int getNextJob(vector<vector<int>>& jobs, int idx, const int& n)
-{
-    for(int i=idx+1; i<n; i++)
-    {
-        if(jobs[idx][1] <= jobs[i][0]) return i;
-    }
-    return n;
-}
-unordered_map<int, int> dp;    // Only store for relavent index
-int maxProfit(vector<vector<int>>& jobs, int idx, const int& n)
-{
-	if(idx == n) return 0;
+    
+   unordered_map < int, int > dp;
+    
+   int maxProfit(vector < vector < int >> & jobs, int idx,const int & n) {
+      if (idx == n) return 0;
 
-	if(dp.find(idx) != dp.end()) return dp[idx];
+      if (dp.find(idx) != dp.end()) return dp[idx];
 
-	// include current job and find the next job with start > curr end time
-	int next = getNextJob(jobs, idx, n);  // Pick any one of the helper function
-	int incl = jobs[idx][2] + maxProfit(jobs, next, n);
+      int next = getNextJob(jobs, idx, n);
+       
+      int including = jobs[idx][2] + maxProfit(jobs, next, n);
 
-	// exclude current job
-	int excl = maxProfit(jobs, idx+1, n);
+      int excluding = maxProfit(jobs, idx + 1, n);
 
-	return dp[idx] = max(incl, excl);
-}
+      return  dp[idx] = max(including, excluding);
+   }
 
-int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    
+   int jobScheduling(vector < int > & startTime, vector < int > & endTime, vector < int > & profit) {
 
-	int n = startTime.size();
-	vector<vector<int>> jobs(n, vector<int>(3));
+      int n = startTime.size();
+      vector < vector < int >> jobs(n, vector < int > (3));
 
-	for(int i=0; i<n; i++)
-	{
-		jobs[i][0] = startTime[i];
-		jobs[i][1] = endTime[i];
-		jobs[i][2] = profit[i];
-	}
+      for (int i = 0; i < n; i++) {
+         jobs[i][0] = startTime[i];
+         jobs[i][1] = endTime[i];
+         jobs[i][2] = profit[i];
+      }
 
-	sort(jobs.begin(), jobs.end());
-	return maxProfit(jobs, 0, n);
-}
+      sort(jobs.begin(), jobs.end());
+      return maxProfit(jobs, 0, n);
+   }
 };
