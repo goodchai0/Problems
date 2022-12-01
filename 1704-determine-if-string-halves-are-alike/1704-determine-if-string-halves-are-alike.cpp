6ca480b4e3995915679d5bf04c600da639d45747
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-12-01 20:55:12 +0530

    Time: 4 ms (73.06%), Space: 6.4 MB (95.88%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,20 +1,19 @@
 class Solution {
-public:
-    bool halvesAreAlike(string s) {
-        int count1=0,count2=0;
-        int mid = size(s) / 2;
-        for(int i=0;i<s.length()/2;i++){
-            if(isVowel(s[i]))
-                count1++;
-            if(isVowel(s[i+mid]))
-                count2++;
-        }
-        if(count1==count2) 
-            return true;
-        else return false;
-            
-    }
-    bool isVowel(char &c){        
-	return c == 'a' || c == 'e' || c =='i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';        
-}
+   public: bool halvesAreAlike(string s) {
+      int count1 = 0, count2 = 0;
+      int mid = size(s) / 2;
+      for (int i = 0; i < mid; i++) {
+         if (isVowel(s[i]))
+            count1++;
+         if (isVowel(s[i + mid]))
+            count2++;
+      }
+      if (count1 == count2)
+         return true;
+      else return false;
+
+   }
+   bool isVowel(char & c) {
+      return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+   }
 };
