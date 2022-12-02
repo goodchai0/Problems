6a522969fc6e111599dd0a0a7eb4ce350862d4b1
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2022-12-02 17:45:40 +0530

    Time: 50 ms (51.77%), Space: 15.4 MB (37.83%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<string,priority_queue<string,vector<string>,greater<string>>>mp;
+    vector<string>ans;
+    
+    void dfs(string s){
+        auto &map=mp[s];
+        while(!map.empty()){
+            string to=map.top();
+            map.pop();
+            dfs(to);
+            
+        }
+        ans.push_back(s);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(auto x:tickets)
+        {
+            mp[x[0]].push(x[1]);
+        }
+        dfs("JFK");
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
