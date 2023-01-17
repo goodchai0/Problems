7a3c3eca7b69c2a5876a28037afa823fe8ec4a91
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-01-18 02:02:50 +0530

    Time: 34 ms (63.71%), Space: 12.8 MB (39.85%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -24,7 +24,32 @@ public:
         }
     }
     int minFlipsMonoIncr(string s) {
-        memset(memo,-1,sizeof(memo));
-        return dp(s,0,0);
+        // memset(memo,-1,sizeof(memo));
+        // return dp(s,0,0);
+        
+        // tabulation
+        
+        int n=s.size();
+        int dp[n+1][2];
+        dp[n][0]=dp[n][1]=0;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==0){
+                    if(s[i]=='1'){
+                    dp[i][j]=min(1+dp[i+1][0],dp[i+1][1]);
+                    }else{
+                        dp[i][j]=min(1+dp[i+1][1],dp[i+1][0]);
+                    }
+                }else{
+                    if(s[i]=='1'){
+                        dp[i][j]=dp[i+1][1];
+                    }else{
+                        dp[i][j]=1+dp[i+1][1];
+                    }
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
