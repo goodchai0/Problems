f8e26ca8394f2c2d4029fb1009d86fd0dce1b38d
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-01-29 00:53:32 +0530

    Time: 27 ms (31.78%), Space: 16.3 MB (12.28%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    class Trie{
+        public:
+            Trie *child[26];
+            bool isEnd=false;
+    };
+    
+    void insert(string &word,Trie* root){
+        Trie *curr=root;
+        for(auto &ch:word){
+            if(!curr->child[ch-'a'])curr->child[ch-'a']=new Trie();
+            curr=curr->child[ch-'a'];
+        }
+        curr->isEnd=true;
+    }
+    
+    bool search(string &word,Trie* root){
+        Trie* curr=root;
+        for(auto &ch:word){
+            if(!curr->child[ch-'a'])return false;
+            curr=curr->child[ch-'a'];
+        }
+        return curr->isEnd;
+    }
+    
+    int dp[305][305];
+    
+    bool solve(string &s, Trie *root,int n,int start){
+        if(start==n)return true;
+        if(dp[start][n]!=-1)return dp[start][n];
+        for(int i=start;i<n;i++){
+            string str=s.substr(start,i-start+1);
+            if(search(str,root)){
+                if(solve(s,root,n,i+1))return dp[start][n]=true;
+            }
+        }
+        return dp[start][n]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Trie* root=new Trie();
+        for(auto &word:wordDict)insert(word,root);
+        
+        int n=s.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(s,root,n,0);
+    }
+};
