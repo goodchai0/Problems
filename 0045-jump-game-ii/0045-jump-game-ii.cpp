0a3f1896f7f97daf3ca3623454ac4506de1b59aa
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-02-08 20:46:53 +0530

    Time: 4 ms (99.69%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,59 +1,14 @@
-// class Solution {
-// public:
-//     int helper(int index,vector<int>& nums,int steps,vector<int>&dp){
-//         if(index==nums.size()-1){
-//             return steps;
-//         }
-        
-//         if(dp[index]!=-1)
-//             return dp[index];
-        
-//         else if(index>=nums.size()){
-//             return INT_MAX;
-//         }
-//         int ans=INT_MAX;
-//         for(int i=1;i<=nums[index];i++){
-//             if(i+index<=nums.size()){
-//                 int res=helper(index+i,nums,steps+1,dp);
-//                 ans=min(res,ans);
-//                 if(res!=INT_MAX)
-//                     dp[index]=ans;
-//             }
-//             else break;
-           
-//         }
-//         return dp[index]=ans;
-//     }
-//     int jump(vector<int>& nums) {
-//         vector<int>dp(nums.size()+1,-1);
-
-//         int ans1=helper(0,nums,0,dp);
-//         return ans1;
-//     }
-// };
 class Solution {
-   
-    int solve(int ind,vector<int>&nums,vector<int>&dp)
-    {
-        if(ind>=nums.size()-1)
-            return 0;    
-        
-        if(dp[ind]!=-1)
-            return dp[ind];   
-        
-        int m=INT_MAX;   
-        for(int i=1;i<=nums[ind];i++)
-        {
-            int a=solve(ind+i,nums,dp);
-            if(a!=INT_MAX)    
-                m=min(a+1,m);
-        }
-        return dp[ind]=m;     
-    }
 public:
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-      return solve(0,nums,dp);     
-        
+        int far=0,cur=0,jumps=0;
+        for(int i=0;i<nums.size()-1;i++){
+            far=max(far,nums[i]+i);
+            if(i==cur){
+                cur=far;
+                jumps++;
+            }
+        }
+        return jumps;
     }
 };
