2ee8b272706be9cae2b6a679cd5ab5d8cf3aa6d1
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-03-21 17:13:07 +0530

    Time: 182 ms (40.12%), Space: 107.6 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                long long j=0;
-                while(i<nums.size() && nums[i]==0){
-                    j++;
-                    i++;
-                }
-                ans+=(j*(j+1))/2;
-            }
-        }
-        return ans;
+long long zeroFilledSubarray(vector<int>& nums) {
+    long long res = 0;
+    for (int i = 0, j = 0; i < nums.size(); ++i) {
+        if (nums[i] != 0)
+            j = i + 1;
+         res += i - j + 1;
     }
+    return res;
+}
 };
+
+// class Solution {
+// public:
+//     long long zeroFilledSubarray(vector<int>& nums) {
+//         long long ans=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0){
+//                 long long j=0;
+//                 while(i<nums.size() && nums[i]==0){
+//                     j++;
+//                     i++;
+//                 }
+//                 ans+=(j*(j+1))/2;
+//             }
+//         }
+//         return ans;
+//     }
+// };
