174082509cb1b8fde5c8243a39405f9bc8f4ee95
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-03-22 15:58:18 +0530

    Time: 463 ms (79.54%), Space: 123.5 MB (83.54%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         vector<pair<int,int>>adj[n+1];
-        
         for(int i=0;i<roads.size();i++){
             adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
             adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
@@ -10,7 +9,7 @@ public:
         
         vector<int>vis(n+1,0);
         queue<int>q;
-        q.push(1),vis[1]=1;
+        q.push(1);vis[1]=1;
         
         while(!q.empty()){
             int node=q.front();
@@ -22,7 +21,8 @@ public:
                 }
             }
         }
-        
+    
+    
         int mini_path=INT_MAX;
         for(int i=0;i<roads.size();i++){
             if(vis[roads[i][0]] && vis[roads[i][1]] && roads[i][2]<mini_path)
