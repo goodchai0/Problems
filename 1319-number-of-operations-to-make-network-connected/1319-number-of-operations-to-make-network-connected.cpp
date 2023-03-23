ca4a1b44102f27ece6ac42a72790e08c5e5d12bc
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-03-23 16:01:33 +0530

    Time: 165 ms (31.40%), Space: 72.1 MB (5.01%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph,vector<bool>&vis,int i){
-        vis[i]=true;
-        for(auto x:graph[i]){
-            if(!vis[x]){
-                dfs(graph,vis,x);
-            }
-        }
-    }
+    // void dfs(vector<vector<int>>& graph,vector<bool>&vis,int i){
+    //     vis[i]=true;
+    //     for(auto x:graph[i]){
+    //         if(!vis[x]){
+    //             dfs(graph,vis,x);
+    //         }
+    //     }
+    // }
     int makeConnected(int n, vector<vector<int>>& connections) {
         
         if(connections.size()+1<n)return -1;
@@ -20,10 +20,28 @@ public:
         vector<bool>vis(n,false);
         int component=0;
         
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]){
+        //         component++;
+        //         dfs(graph,vis,i);
+        //     }
+        // }
+        
         for(int i=0;i<n;i++){
             if(!vis[i]){
+                queue<int>q;
+                q.push(i);
                 component++;
-                dfs(graph,vis,i);
+                while(!q.empty()){
+                    int fnt=q.front();
+                    q.pop();
+                    for(auto x:graph[fnt]){
+                        if(!vis[x]){
+                            vis[x]=true;
+                            q.push(x);
+                        }
+                    }
+                }
             }
         }
         return component-1;
