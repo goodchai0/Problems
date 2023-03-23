660993a14af3e570fdc9e182e519bb2bc0697e9f
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-03-23 15:31:29 +0530

    Time: 197 ms (14.50%), Space: 37.4 MB (96.09%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>& graph,vector<bool>&vis,int start){
-        vis[start]=true;
-        for(auto x:graph[start]){
-            if(!vis[x]){
-                dfs(graph,vis,x);
-            }
-        }
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(n>connections.size()+1)return -1;
-        
-        vector<vector<int>>graph(n);
-        for(auto x:connections){
-            graph[x[0]].push_back(x[1]);
-            graph[x[1]].push_back(x[0]);
+        if(connections.size()+1<n){
+            return -1;
         }
-        int component=0;
-        vector<bool>vis(n,false);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                queue<int>q;
-                q.push(i);
-                component++;
-                while(!q.empty()){
-                    int fnt=q.front();
-                    q.pop();
-                    for(auto x:graph[fnt]){
-                        if(!vis[x]){
-                            vis[x]=true;
-                            q.push(x);
-                        }
-                    }
-                }
-            }
-        }
-        return component-1;
+        vector<int>parent(n,-1);
+        int count=1;
         
+        for(int i=0;i<connections.size();i++){
+            uni(parent,connections[i][0],connections[i][1],count);
+        }
+        return n-count;
+    }
+    int findAbsParent(vector<int>&parent,int i){
+        while(parent[i]!=-1)
+            i=parent[i];
+        return i;
+    }
+    void uni(vector<int>&parent,int x,int y,int& count){
+        int parent1=findAbsParent(parent,x);
+        int parent2=findAbsParent(parent,y);
+        if(parent1!=parent2){
+            parent[parent1]=parent2;
+            count++;
+        }
+
     }
 };
