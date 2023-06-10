2c25e20c6709c26519e2d3d607d1411335d0169d
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-06-11 02:03:21 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (49.60%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void generate(vector<int>& nums,int i,vector<int>& v){
-        if(i==nums.size()){
-            ans.push_back(v);
-            return ;
-        }
+//     void generate(vector<int>& nums,int i,vector<int>& v){
+//         if(i==nums.size()){
+//             ans.push_back(v);
+//             return ;
+//         }
         
-        v.push_back(nums[i]);
+//         v.push_back(nums[i]);
         
-        generate(nums,i+1,v);
-        v.pop_back();            
-        generate(nums,i+1,v);
+//         generate(nums,i+1,v);
+//         v.pop_back();            
+//         generate(nums,i+1,v);
         
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
+//     }
+    vector<vector<int>> subsets(vector<int>& s) {
         vector<int>v;
-        generate(nums,0,v);
-        return ans;
+        // generate(nums,0,v);
+        int n=1<<s.size();
+		    
+		    for(int i=0;i<n;i++){
+		        vector<int>temp;;
+		        for(int j=0;j<s.size();j++){
+		            if(i&(1<<j)){
+		                temp.push_back(s[j]);
+		            }
+		        }
+		        ans.push_back(temp);
+		    }
+		    sort(ans.begin(),ans.end());
+		    return ans;
     }
 };
