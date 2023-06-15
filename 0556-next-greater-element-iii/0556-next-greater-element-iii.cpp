36aeea5922638741f88225b9ecc906acfa724215
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-06-15 14:19:10 +0530

    Time: 3 ms (41.95%), Space: 5.9 MB (68.04%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    int nextGreaterElement(int num) {
-        
-        string str = to_string(num);
-        
-        int n = str.size();
-        
-        int i = n - 2;
-        
-        while(i >= 0 && str[i] >= str[i + 1])
-        {
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        int i=s.length()-2;
+        while(i>=0 && s[i]>=s[i+1]){
             i--;
         }
-        
         if(i < 0)
         {
             return -1;
         }
-        
-        int j = n - 1;
-        
-        while(j >= 0)
-        {
-            if(str[j] > str[i])
-            {
-                break;
-            }
-            
+        int j=s.length()-1;
+        while(j>=0 && s[i]>=s[j]){
             j--;
         }
-        
-        swap(str[i], str[j]);
-        
-        reverse(str.begin() + i + 1, str.end());
-        
-        long long res = stol(str);
-        
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1,s.end());
+        auto res=stol(s);        
         if(res > INT_MAX || res < INT_MIN)
             return -1;
-        
         return res;
     }
 };
