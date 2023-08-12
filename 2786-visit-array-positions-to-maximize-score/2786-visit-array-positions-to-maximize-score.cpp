387abdaecd9461f827a41c71381de96a1580ac4e
Amit Kumar <79858905+goodchai0@users.noreply.github.com>
2023-08-12 20:33:46 +0530

    Time: 311 ms (58.01%), Space: 158.2 MB (43.18%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -13,10 +13,21 @@ public:
         dp[i][p]=max(notTake,take);
         return dp[i][p];
     }
+    
+    
     long long maxScore(vector<int>& nums, int x) {
         int n=nums.size();
-        vector<vector<long long>>dp(n,vector<long long>(2,-1));
-        helper(nums,0,n,nums[0]%2,x,dp);
-        return max(dp[0][1],dp[0][0]);
+        vector<vector<long long>>dp(n+1,vector<long long>(2,0));        
+        for(int i=n-1;i>=0;i--){
+            for(int p = 0; p <= 1; p++) {
+                long long take=nums[i]+dp[i+1][nums[i]%2];
+                if(i>0 && nums[i]%2!=p){
+                    take-=x;
+                }
+                long long notTake=dp[i+1][p];
+                dp[i][p]=max(notTake,take);
+            }
+        }
+        return min(dp[0][1],dp[0][0]);
     }
 };
